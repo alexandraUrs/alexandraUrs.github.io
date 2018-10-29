8435af1b66e05d79831b4ca70f0ed9be40be3588
alexandraUrs <44163851+alexandraUrs@users.noreply.github.com>
2018-10-29 13:47:31 +0200

    Update html5/desenare.js

--- html5/desenare.js
+++ html5/desenare.js
@@ -32,7 +32,7 @@ function start()
 	document.getElementById("id_start_button").disabled = true;
 	document.getElementById("id_stop_button").disabled = false;
 	
-	var my_worker=new Worker("calcul _prime.js");
+	var my_worker=new Worker("calcul_prime.js");
 	my_worker.onmessage = function(e){
 	  document.getElementById("id_prime").innerHTML = e.data;	
 	} 
