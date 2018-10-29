2c7244b85f432d6dc9118e42ed1b90b7075d19b5
alexandraUrs <44163851+alexandraUrs@users.noreply.github.com>
2018-10-29 15:59:57 +0200

    orient

--- html5/orientation.js
+++ html5/orientation.js
@@ -1,10 +1,20 @@
-document.getElementById("id_logic_version").innerHTML="Logic version: 2018.10.29.0";
+document.getElementById("id_logic_version").innerHTML="Logic version: 2018.10.29.1";
 window.addEventListener("deviceorientation",on_device_orientation);
 
 function on_device_orientation(e)
 {
-	document.getElementById("id_alpha"e).innerHTML = e.alpha;
-	document.getElementById("id_beta"e).innerHTML = e.beta;
-	document.getElementById("id_gamma"e).innerHTML = e.gamma;
+	document.getElementById("id_alpha").innerHTML = e.alpha;
+	document.getElementById("id_beta").innerHTML = e.beta;
+	document.getElementById("id_gamma").innerHTML = e.gamma;
 	
+	var vanvas = document.getElementById("id_canvas");
+	var context = cnvas.getContext("2d");
+	
+	context.clearRect(0, 0, canvas.width, canvas.height)
+	var R=10;
+	context.beginPath();
+	context.rc(canvas.width /2 +e.gamma/90*(canvas.width/2 -R), 
+	           canvas.height/2 +e.beta/90*(cavanas.height/2 -R),
+			   R, 0, 2*Math.PI );
+	context.stroke();
 }
