ed55cb68840df24fca636a4348190a2619f78680
alexandraUrs <44163851+alexandraUrs@users.noreply.github.com>
2018-10-29 15:41:10 +0200

    Update html5/orientation.js

--- html5/orientation.js
+++ html5/orientation.js
@@ -1,7 +1,7 @@
 document.getElementById("id_logic_version").innerHTML="Logic version: 2018.10.29.0";
 window.addEventListener("deviceorientation",on_device_orientation);
 
-function on_device_orientation
+function on_device_orientation(e)
 {
 	document.getElementById("id_alpha"e).innerHTML = e.alpha;
 	document.getElementById("id_beta"e).innerHTML = e.beta;
