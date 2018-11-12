7d3a810ff04bed80aab25cc50fa108f8b3cee296
alexandraUrs <44163851+alexandraUrs@users.noreply.github.com>
2018-11-12 14:41:49 +0200

    1

--- html5/cam.js
+++ html5/cam.js
@@ -1,5 +1,8 @@
-document.getElementById("id_logic_version").innerHTML="Logic version: 2018.11.12.1";
+document.getElementById("id_logic_version").innerHTML="Logic version: 2018.11.12.2";
 document.getElementById("id_button").addEventListener("click", start);
+document.getElementById("is_canvas").addEventListener("touchstart", on_touch);
+document.getElementById("is_canvas").addEventListener("mousedown", on_touch);
+
 
 function on_success(e)
 {
@@ -15,4 +18,12 @@ function start()
 {
 	var c = {audio:true, video: {facingMode:"environment"}};
 	navigator.mediaDevices.getUserMedia(c).then(on_success).catch(on_failure);
+}
+//----------------------
+function on_touch(e)
+{
+	var canvas = document.getElementById("id_canvas");
+	var context = canvas.getContext("2d");
+	var video =document.getElementById("id_video");
+	context.drawImage(video, 0, 0 );
 }
