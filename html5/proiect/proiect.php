305bfa557ddf380ae4bc0971fe2329bddbf7541f
alexandraUrs <44163851+alexandraUrs@users.noreply.github.com>
2019-01-19 22:16:46 +0200

    proiect

---
+++ html5/proiect/proiect.php
@@ -0,0 +1,86 @@
+<?php
+include "./includes/head.php";
+include "./includes/antet.php";
+//include "./includes/meniu_lateral.php";
+?>
+
+
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+
+
+
+<div class="container">
+      <div class="row">
+      <div class="col-md-5  toppad  pull-right col-md-offset-3 ">
+          
+           <p class=" text-info"><?php echo $datacurenta?></p>
+      </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
+        <div class="panel panel-info">
+            <div class="panel-heading">
+              <h3 class="panel-title">Tabel pacienti:</h3>
+            </div>
+            <div class="panel-body">
+              
+			  
+			<?php
+               $comanda = "SELECT * FROM pacienti";
+               $rezultat = mysql_query($comanda);
+               //$row = mysql_fetch_assoc($rezultat);
+            ?>       	  
+  
+			 
+			 
+                
+                  <table class="table table-user-information">
+                   <thead>
+                      <tr>
+                        <th>Nume:</th>
+						<th>Prenume:</th>
+						<th>Rp medic:</th>
+						<th>Rp eliberata:</th>
+						
+                      </tr>
+					  </thead>
+					  
+					  <tbody>
+					  
+                         <?php
+                           if (mysql_num_rows($rezultat) > 0) {
+                            while($row = mysql_fetch_assoc($rezultat)) { 
+							    echo "<tr>";
+								echo "<td>" . $row["nume"] . "</td>";
+								echo "<td>" . $row["prenume"] . "</td>";
+                                
+								echo "<td>" . $row["rp_medic"] . "</td>";
+								echo "<td>" . $row["rp_eliberata"] . "</td>";
+                                echo "</tr>";
+                                }
+						   
+                            } else {
+                                echo "<tr><td colspan='8'>Nu aveti utilizatori</td></tr>";
+                            }
+                           ?>
+						
+                      </tbody>
+                  </table>  
+                     <a href="proiect1.php" class="btn btn-primary">Adaugare pacient </a>				  
+                  </div>
+    
+                 <div class="panel-footer">
+                 </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+<?php
+include "./includes/footer.php";
+
+?>
---
+++ html5/proiect/proiect1.php
@@ -0,0 +1,156 @@
+<?php
+include "./includes/head.php";
+include "./includes/antet.php";
+//include "./includes/meniu_lateral.php";
+?>
+
+<style>
+body {
+    font-size: 19px;
+}
+
+
+form {
+    width: 80%;
+    margin: 50px auto;
+    text-align: left;
+    padding: 20px; 
+    border: 1px solid #bbbbbb; 
+    border-radius: 5px;
+}
+
+.input-group {
+    margin: 10px 0px 10px 0px;
+}
+.input-group label {
+    display: block;
+    text-align: left;
+    margin: 3px;
+}
+.input-group input {
+    height: 30px;
+    width: 93%;
+    padding: 5px 10px;
+    font-size: 16px;
+    border-radius: 5px;
+    border: 1px solid gray;
+}
+.btn {
+    padding: 10px;
+    font-size: 15px;
+    color: white;
+    background: #5F9EA0;
+    border: none;
+    border-radius: 5px;
+}
+.edit_btn {
+    text-decoration: none;
+    padding: 2px 5px;
+    background: #2E8B57;
+    color: white;
+    border-radius: 3px;
+}
+
+</style>
+
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+
+
+
+<div class="container">
+      <div class="row">
+      <div class="col-md-5  toppad  pull-right col-md-offset-3 ">
+          
+           <p class=" text-info"><?php echo $datacurenta?></p>
+      </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
+
+     	  
+  
+ 
+ 
+       <div class="panel panel-info">
+            <div class="panel-heading">
+              <h3 class="panel-title">Adaugare pacient:</h3>
+            </div>
+		
+            <div class="panel-body">
+          
+                  <?php
+
+                       if (isset($_POST['submit'])) {
+						
+                            //daca s-a efectuat trimiterea formularului
+                            //actualizăm înregistrarea în baza de date
+                         $nume=$_POST['nume'];
+                         $prenume = $_POST['prenume'];
+					     $rp_medic = $_POST['rp_medic'];
+						 $rp_eliberata = $_POST['rp_eliberata'];
+						 
+						 $com = "INSERT INTO pacienti (nume,prenume,rp_medic,rp_eliberata)
+                                VALUES ('$nume','$prenume','$rp_medic','$rp_eliberata');";
+						 
+                         $result= mysql_query($com);
+					   
+						 if (!$result) {
+                            	echo mysql_error();
+                          } else {
+                             	echo "<h2>Adaugare efectuata cu succes!</h2>";
+                                echo "<p>Înapoi la <a href='proiect.php'>Proiect</a>";
+                            }
+					   
+					   }else{
+					  ?>		  
+			  			
+			  
+			  
+                        <form  action="proiect1.php" method="post">
+                                <div class="input-group">
+                                    <label>Nume:</label>
+                                    <input type="text" name="nume" id="nume" value="" >
+                                </div class="input-group">
+                                <div class="input-group">
+                                    <label> Prenume :</label>
+                                    <input type="text" name="prenume" id="prenume"  value="" >
+                                </div>
+                                <div class="input-group">
+                                    <label>Rp medic:</label>
+                                    <input type="text" name="rp_medic" id="rp_medic" value="" >
+                                </div>
+								<div class="input-group">
+                                    <label>Rp eliberata:</label>
+                                    <input type="text" name="rp_eliberata" id="rp_eliberata" value="" >
+                                </div>
+                                <div class="input-group">
+                                  <button class="btn" type="submit" name="submit" >Submit</button>
+                                </div>
+                        </form>     
+			
+	   <div class="panel-footer">
+       </div>
+	   
+	    <?php
+
+                        }
+                        mysql_close($conectare);
+
+
+         ?>
+	   
+    </div>
+  </div>
+ </div>	
+</div>		
+
+
+
+
+
+<?php
+include "./includes/footer.php";
+
+?>
