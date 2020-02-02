f9295d59036a55841ba7dabc97f6c01044665456
alexandraUrs <44163851+alexandraUrs@users.noreply.github.com>
2020-02-02 18:47:42 +0200

    m

--- html5/prooiect/js/cam.js
+++ html5/prooiect/js/cam.js
@@ -1,7 +1,7 @@
-document.getElementById("id_button").addEventListener("click", fa poza );
+document.getElementById("id_button").addEventListener("click", start );
 document.getElementById("id_video").addEventListener("touchstart", on_touch);
 document.getElementById("id_video").addEventListener("mousedown", on_touch);
-
+document.getElementById("myCanvas").addEventListener("click", start );
 
 function on_success(e)
 {
@@ -25,4 +25,11 @@ function on_touch(e)
 	var context = canvas.getContext("2d");
 	var video =document.getElementById("id_video");
 	context.drawImage(video, 0, 0);
+}
+
+function myCanvas() {
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  var img = document.getElementById("med");
+  ctx.drawImage(img,10,10);
 }
